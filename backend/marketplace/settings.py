c845d0ffd01697c96899499210387b2e7fdc35b9
Amir Afshar <afshar_amir_ar@yahoo.com>
2025-08-06 19:04:48 +0330

    feat: fixed environment variable sample according to readme

--- backend/marketplace/settings.py
+++ backend/marketplace/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3%xdq)2mespa_bt6pcxc^cglpys_5%(uz@k@8(y4f8jrw$9p5p')
+load_dotenv()  # This will load variables from .env
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
